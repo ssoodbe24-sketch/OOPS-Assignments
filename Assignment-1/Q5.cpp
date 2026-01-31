adb8756495462da4c8e50245a7773c32e2d6b9ee
ssoodbe24-sketch <ssood_be24@thapar.edu>
2026-01-31 22:41:34 +0530

    Create Q5.cpp

---
+++ Assignment-1/Q5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Hello\nWorld\n";
+    cout << "Surya\tSood\n";
+    cout << "Surya\bS\n";
+    cout << setw(10) << "Sood";
+    return 0;
+}
