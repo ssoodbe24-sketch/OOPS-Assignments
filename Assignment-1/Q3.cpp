77225c86bac1dab6d03fccecf800f168299fb018
ssoodbe24-sketch <ssood_be24@thapar.edu>
2026-01-31 22:39:35 +0530

    Create Q3.cpp

---
+++ Assignment-1/Q3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    cout << "Add = " << a+b << endl;
+    cout << "Sub = " << a-b << endl;
+    cout << "Mul = " << a*b << endl;
+    cout << "Div = " << a/b << endl;
+    return 0;
+}
