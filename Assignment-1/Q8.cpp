a1916e42cda7249bde9f59c3d83f4e44f4ff78b2
ssoodbe24-sketch <ssood_be24@thapar.edu>
2026-01-31 22:43:49 +0530

    Create Q8.cpp

---
+++ Assignment-1/Q8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d;
+    cin >> d;
+
+    if(d <= 5)
+        cout << "Fine = 50 paise";
+    else if(d <= 10)
+        cout << "Fine = 1 rupee";
+    else if(d <= 30)
+        cout << "Fine = 5 rupees";
+    else
+        cout << "Membership Cancelled";
+
+    return 0;
+}
