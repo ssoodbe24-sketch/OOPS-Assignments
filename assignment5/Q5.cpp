617286303161beb2f1e37ba4ba269203dfb96d6b
ssoodbe24-sketch <ssood_be24@thapar.edu>
2026-02-16 16:26:57 +0530

    Create Q5.cpp

---
+++ assignment5/Q5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Base {
+public:
+    Base() {
+        cout << "Base class constructor called" << endl;
+    }
+    ~Base() {
+        cout << "Base class destructor called" << endl;
+    }
+};
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived class constructor called" << endl;
+    }
+    ~Derived() {
+        cout << "Derived class destructor called" << endl;
+    }
+};
+int main() {
+    cout << "Creating Derived object..." << endl;
+    Derived obj;
+    cout << "End of main function" << endl;
+    return 0;
+}
